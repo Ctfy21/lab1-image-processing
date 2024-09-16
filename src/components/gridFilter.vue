7b964d577df216b610985b36dd14ef1b6c3ee55a
Ctfy21 <skileev@bk.ru>
2024-09-16 22:58:53 +0300

    omg

--- src/components/gridFilter.vue
+++ src/components/gridFilter.vue
@@ -1,20 +1,56 @@
 <template>
-    <v-otp-input :model-value="otp1" :length="3"></v-otp-input>
-    <v-otp-input :model-value="otp2" :length="3"></v-otp-input>
-    <v-otp-input :model-value="otp3" :length="3"></v-otp-input>
+<!-- <div class="otp" id="otp">
+    <input v-model="filterAlg.values[0]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[1]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[2]" type="text" class="otp__input" maxlength="2" />
+</div>
+<div class="otp" id="otp">
+    <input v-model="filterAlg.values[3]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[4]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[5]" type="text" class="otp__input" maxlength="2" />
+</div>
+<div class="otp" id="otp">
+    <input v-model="filterAlg.values[6]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[7]" type="text" class="otp__input" maxlength="2" />
+    <input v-model="filterAlg.values[8]" type="text" class="otp__input" maxlength="2" />
+</div> -->
 </template>   
-  
+
+<style lang="css">
+    .otp {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+    }
+    .otp__input {
+        border: none;
+        border: 2px solid rgb(203 213 225);
+        font-size: 2rem;
+        outline: none;
+        padding: 0.25rem 0.5rem;
+        text-align: center;
+        width: 5rem;
+    }
+</style>
+
 <script>
     export default{
+        props: {
+            filterAlg: {
+                type: Object,
+                }
+            },
         data(){
             return{
-                otp1: "111",
-                otp2: "111",
-                otp3: "111"
+
             }
         },
         mounted(){
 
         },
+        methods:{
+
+        }
     }
 </script>
--- src/components/popupFilterImage.vue
+++ src/components/popupFilterImage.vue
@@ -1,22 +1,21 @@
 <template>
-    <v-dialog maxWidth="1000" v-model="dialogFilter">
+    <v-dialog maxWidth="1000">
         <template v-slot:activator="{ props: activatorProps }">
             <v-btn
             v-bind="activatorProps"
             color="green"
             text="Filter correction"
             variant="flat"
-            @click="dialogFilter = true"
             ></v-btn>
         </template>
         <template v-slot:default="{ isActive }">
             <v-card title="Filter correction">
                 <v-card-item align="center" justify="center">
                 
-                <GridFilter></GridFilter>
+                <GridFilter :filterAlg="filterAlg"></GridFilter>
 
                 <v-row class="mt-10">
-                    <v-select label="Type of filter alghorotm" :items="filterAlgList" :v-model="filterAlg" item-title="title"/>
+                    <v-select label="Type of filter alghorotm" :items="filterAlgList" v-model="filterAlg" item-title="title" item-value="values"/>
                 </v-row>
                 </v-card-item>
                 <v-card-actions>
@@ -24,13 +23,12 @@
                 <v-btn
                     text="Reset"
                     color="blue"
-                    @click="dialogFilter = false"
                 ></v-btn>
 
                 <v-btn
                     text="Close"
                     color="red"
-                    @click="dialogFilter = false"
+                    @click="isActive.value = false"
                 ></v-btn>
 
                 <v-btn
@@ -57,23 +55,53 @@ export default {
             filterAlgList: [
                 {
                     title: 'Base variant',
-                    values: []
+                    values: [1,1,1,1,1,1,1,1,1]
                 },
                 {
                     title: 'Increase sharpness',
-                    values: []
+                    values: [0,1,0,1,-4,1,0,1,0]
                 },
                 {
                     title: 'Gaussian filter',
-                    values: []
+                    values: this.makeGaussKernel(3)
                 },
                 {
-                    title: 'Rectangle filter',
-                    values: []
+                    title: 'Median filter',
+                    values: [1,1,1,1,1,1,1,1,1]
                 }
             ],
-            filterAlg: null
-        } 
+            filterAlg: {
+                    title: 'Base variant',
+                    values: [1,1,1,1,1,1,1,1,1]
+                },
+        }
     },
+    methods:{
+        implementFilter(newArray){
+                for(let i = 0; i < this.arrayWeightsInit.length; i++){
+                    this.arrayWeightsInit[i] = newArray[i]
+                }
+            },
+        makeGaussKernel(sigma){
+            const GAUSSKERN = 3.0;
+            var dim = parseInt(Math.max(3.0, GAUSSKERN * sigma));
+            var sqrtSigmaPi2 = Math.sqrt(Math.PI*2.0)*sigma;
+            var s2 = 2.0 * sigma * sigma;
+            var sum = 0.0;
+            
+            var kernel = new Float32Array(dim - !(dim & 1)); // Make it odd number
+            const half = parseInt(kernel.length / 2);
+            for (var j = 0, i = -half; j < kernel.length; i++, j++) 
+            {
+                kernel[j] = Math.exp(-(i*i)/(s2)) / sqrtSigmaPi2;
+                sum += kernel[j];
+            }
+            // Normalize the gaussian kernel to prevent image darkening/brightening
+            for (var i = 0; i < dim; i++) {
+                kernel[i] /= sum;
+            }
+            return kernel;
+            },
+    }
 }
 </script>
